1932f80ca99b60ad7454ba8b6b006a18c55550e8
Mats Liljegren <mats@mexit.se>
2026-07-07 12:08:23 +0200

    Add validation workflow examples

---
+++ crates/mpi/examples/priority_shutdown.rs
@@ -0,0 +1,38 @@
+use mpi::task;
+use std::sync::{Arc, Mutex};
+
+#[derive(Default)]
+struct Worker {
+    log: Option<Arc<Mutex<Vec<&'static str>>>>,
+}
+
+#[task(queue_size = 8)]
+impl Worker {
+    #[start]
+    async fn start(&mut self, _ctx: &mut WorkerContext, log: Arc<Mutex<Vec<&'static str>>>) {
+        log.lock().unwrap().push("start");
+        self.log = Some(log);
+    }
+
+    #[event]
+    async fn work(&mut self, _ctx: &mut WorkerContext) {
+        self.log.as_ref().unwrap().lock().unwrap().push("work");
+    }
+
+    #[event(priority)]
+    async fn shutdown(&mut self, ctx: &mut WorkerContext) {
+        self.log.as_ref().unwrap().lock().unwrap().push("shutdown");
+        ctx.stop();
+    }
+}
+
+fn main() {
+    let log = Arc::new(Mutex::new(Vec::new()));
+    let (worker, runtime) = Worker::spawn(Worker::default(), log.clone()).unwrap();
+
+    worker.work_blocking().unwrap();
+    worker.shutdown_blocking().unwrap();
+    runtime.join().unwrap();
+
+    assert_eq!(log.lock().unwrap().as_slice(), &["start", "shutdown"]);
+}
